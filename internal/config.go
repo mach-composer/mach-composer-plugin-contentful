e70926038095cc484afc57cd286f94708d24b805
Nando Davis <nandodavis@gmail.com>
2025-05-15 16:09:33 +0200

    Enhance Contentful configuration: added 'environment' field and updated JSON schemas; updated GitHub Actions to use latest artifact upload action.

--- internal/config.go
+++ internal/config.go
@@ -3,12 +3,16 @@ package internal
 type ContentfulConfig struct {
 	CMAToken       string `mapstructure:"cma_token"`
 	OrganizationID string `mapstructure:"organization_id"`
+	Environment    string `mapstructure:"environment"`
 }
 
 func (c *ContentfulConfig) extendConfig(o *ContentfulConfig) *ContentfulConfig {
 	cfg := &ContentfulConfig{
 		CMAToken:       o.CMAToken,
 		OrganizationID: o.OrganizationID,
+	}
+	if c.Environment != "" {
+		cfg.Environment = c.Environment
 	}
 	if c.CMAToken != "" {
 		cfg.CMAToken = c.CMAToken
