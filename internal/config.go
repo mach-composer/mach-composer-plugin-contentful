0a79995f1e5c6352f3002b87a0992984b055e858
Nando Davis <nandodavis@gmail.com>
2025-05-15 16:00:38 +0200

    Refactor Contentful configuration: removed fields and updated schema

--- internal/config.go
+++ internal/config.go
@@ -1,24 +1,14 @@
 package internal
 
 type ContentfulConfig struct {
-	Space          string `mapstructure:"space"`
-	DefaultLocale  string `mapstructure:"default_locale"`
-	CMAToken       string `mapstructure:"default_locale"`
+	CMAToken       string `mapstructure:"cma_token"`
 	OrganizationID string `mapstructure:"organization_id"`
 }
 
 func (c *ContentfulConfig) extendConfig(o *ContentfulConfig) *ContentfulConfig {
 	cfg := &ContentfulConfig{
-		Space:          o.Space,
-		DefaultLocale:  o.DefaultLocale,
 		CMAToken:       o.CMAToken,
 		OrganizationID: o.OrganizationID,
-	}
-	if c.Space != "" {
-		cfg.Space = c.Space
-	}
-	if c.DefaultLocale != "" {
-		cfg.DefaultLocale = c.DefaultLocale
 	}
 	if c.CMAToken != "" {
 		cfg.CMAToken = c.CMAToken
--- internal/plugin.go
+++ internal/plugin.go
@@ -107,29 +107,9 @@ func (p *Plugin) TerraformRenderResources(site string) (string, error) {
 	}
 
 	template := `
-		provider "contentful" {
+		  provider "contentful" {
 			cma_token       = {{ .CMAToken|printf "%q" }}
 			organization_id = {{ .OrganizationID|printf "%q" }}
-		  }
-
-		  resource "contentful_space" "space" {
-			name           = {{ .Space|printf "%q" }}
-			default_locale = {{ .DefaultLocale|printf "%q" }}
-		  }
-
-		  resource "contentful_apikey" "apikey" {
-			space_id = contentful_space.space.id
-
-			name        = "frontend"
-			description = "MACH generated frontend API key"
-		  }
-
-		  output "contentful_space_id" {
-			value = contentful_space.space.id
-		  }
-
-		  output "contentful_apikey_access_token" {
-			value = contentful_apikey.apikey.access_token
 		  }
 	`
 	return helpers.RenderGoTemplate(template, cfg)
@@ -141,9 +121,7 @@ func (p *Plugin) RenderTerraformComponent(site string, component string) (*schem
 		return nil, nil
 	}
 
-	result := &schema.ComponentSchema{
-		Variables: "contentful_space_id = contentful_space.space.id",
-	}
+	result := &schema.ComponentSchema{}
 	return result, nil
 }
 
